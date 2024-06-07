fd893eb79264c1ca85db71e32e88d3f7aa56c605
dheuvjoshi@gmail.com <dheuvjoshi@gmail.com>
2024-06-07 10:43:51 +0530

    component added

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "../Components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <Router>
+      <>
+        <Header />
+        <Routes />
+        <Footer />
+      </>
+    </Router>
   );
 }
 
